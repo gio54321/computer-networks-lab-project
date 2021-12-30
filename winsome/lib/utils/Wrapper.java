548140811febbb453b565cd1cc8520763411ee3c
gio54321 <g.dellimmagine@gmail.com>
2021-12-30 20:06:07 +0100

    Add wrapper

---
+++ winsome/lib/utils/Wrapper.java
@@ -0,0 +1,18 @@
+package winsome.lib.utils;
+
+public class Wrapper<T> {
+    private T value;
+
+    public Wrapper(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+}
