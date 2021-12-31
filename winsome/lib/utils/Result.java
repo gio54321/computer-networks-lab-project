5dd85c57c3ed9cd49f3b7fb475380e2f8d5b1f32
gio54321 <g.dellimmagine@gmail.com>
2021-12-31 19:26:28 +0100

    Add Result type

---
+++ winsome/lib/utils/Result.java
@@ -0,0 +1,46 @@
+package winsome.lib.utils;
+
+public class Result<T, E> {
+    // loosely inspired by https://doc.rust-lang.org/std/result/
+    private T okValue;
+    private E errValue;
+
+    static <T, E> Result<T, E> ok(T okValue) {
+        var res = new Result<T, E>();
+        res.setOkValue(okValue);
+        return res;
+    }
+
+    static <T, E> Result<T, E> err(E okValue) {
+        var res = new Result<T, E>();
+        res.setErrValue(okValue);
+        return res;
+    }
+
+    public T getOkValue() {
+        return okValue;
+    }
+
+    public void setOkValue(T okValue) {
+        this.okValue = okValue;
+        this.errValue = null;
+    }
+
+    public E getErrValue() {
+        return errValue;
+    }
+
+    public void setErrValue(E errValue) {
+        this.errValue = errValue;
+        this.okValue = null;
+    }
+
+    public boolean isOk() {
+        return this.okValue != null;
+    }
+
+    public boolean isErr() {
+        return this.errValue != null;
+    }
+
+}
