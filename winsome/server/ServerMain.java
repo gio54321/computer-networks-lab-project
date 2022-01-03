5f29250b0fa68d72183437f49f222a2bf1537250
gio54321 <g.dellimmagine@gmail.com>
2022-01-03 10:52:39 +0100

    Remove dummy database

--- winsome/server/ServerMain.java
+++ winsome/server/ServerMain.java
@@ -11,16 +11,12 @@ import winsome.lib.router.InvalidRouteAnnotationException;
 import winsome.lib.router.Router;
 import winsome.server.database.AuthenticationImpl;
 import winsome.server.database.Database;
-import winsome.server.database.User;
-import winsome.server.database.exceptions.UserAlreadyExistsException;
 
 public class ServerMain {
     public static void main(String[] args) {
         try {
+            setupRMI();
             var database = new Database();
-
-            dummyDb(database);
-
             var logic = new RESTLogic(database);
             var auth = new AuthenticationImpl(database);
             var router = new Router(logic, auth);
@@ -32,26 +28,13 @@ public class ServerMain {
         }
     }
 
-    public static void dummyDb(Database db) {
-        String[] tags = { "sports" };
-        var user1 = new User("gio", "aaa", tags);
-        var user2 = new User("tom", "bbb", tags);
-        try {
-            db.registerUser(user1);
-            db.registerUser(user2);
-        } catch (UserAlreadyExistsException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
     public static void setupRMI() {
 
         var usersDatabase = new Database();
 
         // TODO port
         // TODO regostry host
-        var registryPort = 1234;
+        var registryPort = 1235;
         var registrationImpl = new RegistrationImpl(usersDatabase);
         try {
             var stub = (Registration) UnicastRemoteObject.exportObject(registrationImpl, 0);
