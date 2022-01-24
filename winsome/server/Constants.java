1d22d8df3aa3bc064423598612e666fee4ee9752
gio54321 <g.dellimmagine@gmail.com>
2022-01-24 16:39:16 +0100

    Fix small error in constants

--- winsome/server/Constants.java
+++ winsome/server/Constants.java
@@ -2,5 +2,5 @@ package winsome.server;
 
 public class Constants {
     public static int MAX_POST_TITLE_LENGTH = 20;
-    public static int MAX_POST_CONTENT_LENGTH = 140;
+    public static int MAX_POST_CONTENT_LENGTH = 500;
 }
