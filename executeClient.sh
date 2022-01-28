fe9a56efdd90045bddb42cf0bd667771d1188011
gio54321 <g.dellimmagine@gmail.com>
2022-01-28 19:48:05 +0100

    Update execution scripts

--- executeClient.sh
+++ executeClient.sh
@@ -2,4 +2,4 @@
 javac -g -cp jackson/jackson-core-2.9.7.jar:jackson/jackson-databind-2.9.7.jar:jackson/jackson-annotations-2.9.7.jar:. -d bin \
     winsome/client/ClientMain.java \
     && java -cp jackson/jackson-core-2.9.7.jar:jackson/jackson-databind-2.9.7.jar:jackson/jackson-annotations-2.9.7.jar:bin \
-    winsome.client.ClientMain
+    winsome.client.ClientMain config/clientConfig.json
--- executeServer.sh
+++ executeServer.sh
@@ -2,4 +2,4 @@
 javac -g -cp jackson/jackson-core-2.9.7.jar:jackson/jackson-databind-2.9.7.jar:jackson/jackson-annotations-2.9.7.jar:. -d bin \
     winsome/server/ServerMain.java \
     && java -cp jackson/jackson-core-2.9.7.jar:jackson/jackson-databind-2.9.7.jar:jackson/jackson-annotations-2.9.7.jar:bin \
-    winsome.server.ServerMain
+    winsome.server.ServerMain config/serverConfig.json
